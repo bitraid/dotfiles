02f77c6e0ba594f9f1750596d1712bd8054e921b
bitraid <bitraid@protonmail.ch>
2024-04-05 20:37:33 +0300

    qutebrowser: add config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,148 @@
+config.load_autoconfig(False)
+c.backend = 'webengine'
+c.colors.completion.category.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #073642, stop:1 #002B36)'
+c.colors.completion.category.border.bottom = '#002B36'
+c.colors.completion.category.border.top = '#073642'
+c.colors.completion.category.fg = '#93A1A1'
+c.colors.completion.even.bg = '#002B36'
+c.colors.completion.fg = '#657B83'
+c.colors.completion.item.selected.bg = '#586E75'
+c.colors.completion.item.selected.border.bottom = '#002B36'
+c.colors.completion.item.selected.border.top = '#073642'
+c.colors.completion.item.selected.fg = '#002B36'
+c.colors.completion.item.selected.match.fg = '#859900'
+c.colors.completion.match.fg = '#2AA198'
+c.colors.completion.odd.bg = '#073642'
+c.colors.completion.scrollbar.bg = '#002B36'
+c.colors.completion.scrollbar.fg = '#657B83'
+c.colors.contextmenu.disabled.bg = '#002B36'
+c.colors.contextmenu.disabled.fg = '#586E75'
+c.colors.contextmenu.menu.bg = '#002B36'
+c.colors.contextmenu.menu.fg = '#93A1A1'
+c.colors.contextmenu.selected.bg = '#93A1A1'
+c.colors.contextmenu.selected.fg = '#002B36'
+c.colors.downloads.bar.bg = '#073642'
+c.colors.downloads.error.bg = '#002B36'
+c.colors.downloads.error.fg = '#D33682'
+c.colors.downloads.start.bg = '#002B36'
+c.colors.downloads.start.fg = '#6C71C4'
+c.colors.downloads.stop.bg = '#002B36'
+c.colors.downloads.stop.fg = '#268BD2'
+c.colors.hints.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 rgba(7, 54, 66, 0.8), stop:1 rgba(0, 43, 54, 0.8))'
+c.colors.hints.fg = '#FDF6E3'
+c.colors.hints.match.fg = '#B58900'
+c.colors.keyhint.bg = 'rgba(0, 43, 53, 80%)'
+c.colors.keyhint.fg='#839496'
+c.colors.keyhint.suffix.fg = '#B58900'
+c.colors.messages.error.bg = '#073642'
+c.colors.messages.error.border = '#586E75'
+c.colors.messages.error.fg = '#DC322F'
+c.colors.messages.info.bg = '#073642'
+c.colors.messages.info.border = '#586E75'
+c.colors.messages.info.fg = '#B58900'
+c.colors.messages.warning.bg = '#073642'
+c.colors.messages.warning.border = '#586E75'
+c.colors.messages.warning.fg = '#CB4B16'
+c.colors.prompts.bg = '#002B36'
+c.colors.prompts.border = '1px solid #586E75'
+c.colors.prompts.fg = '#93A1A1'
+c.colors.prompts.selected.bg = '#586E75'
+c.colors.statusbar.caret.bg = '#002b36'
+c.colors.statusbar.caret.fg = '#B58900'
+c.colors.statusbar.caret.selection.bg = '#002B36'
+c.colors.statusbar.caret.selection.fg = '#268BD2'
+c.colors.statusbar.command.bg = '#073642'
+c.colors.statusbar.command.fg = '#93A1A1'
+c.colors.statusbar.command.private.bg = '#6C71C4'
+c.colors.statusbar.command.private.fg = '#93A1A1'
+c.colors.statusbar.insert.bg = '#002B36'
+c.colors.statusbar.insert.fg = '#B58900'
+c.colors.statusbar.normal.bg = '#002B36'
+c.colors.statusbar.normal.fg = '#93A1A1'
+c.colors.statusbar.passthrough.bg = '#002b36'
+c.colors.statusbar.passthrough.fg = '#B58900'
+c.colors.statusbar.private.bg = '#6C71C4'
+c.colors.statusbar.private.fg = '#93A1A1'
+c.colors.statusbar.progress.bg = '#93A1A1'
+c.colors.statusbar.url.error.fg = '#DC322F'
+c.colors.statusbar.url.fg = '#93A1A1'
+c.colors.statusbar.url.hover.fg = '#268BD2'
+c.colors.statusbar.url.success.http.fg = '#2AA198'
+c.colors.statusbar.url.success.https.fg = '#859900'
+c.colors.statusbar.url.warn.fg = '#D33682'
+c.colors.tabs.bar.bg = '#002B36'
+c.colors.tabs.even.bg = '#073642'
+c.colors.tabs.even.fg = '#839496'
+c.colors.tabs.indicator.error = '#DC322F'
+c.colors.tabs.indicator.start = '#B58900'
+c.colors.tabs.indicator.stop = '#2AA198'
+c.colors.tabs.indicator.system = 'rgb'
+c.colors.tabs.odd.bg = '#002B36'
+c.colors.tabs.odd.fg = '#839496'
+c.colors.tabs.pinned.even.bg = '#073642'
+c.colors.tabs.pinned.even.fg = '#839496'
+c.colors.tabs.pinned.odd.bg = '#002B36'
+c.colors.tabs.pinned.odd.fg = '#839496'
+c.colors.tabs.pinned.selected.even.bg = '#839496'
+c.colors.tabs.pinned.selected.even.fg = '#002B36'
+c.colors.tabs.pinned.selected.odd.bg = '#839496'
+c.colors.tabs.pinned.selected.odd.fg = '#002B36'
+c.colors.tabs.selected.even.bg = '#839496'
+c.colors.tabs.selected.even.fg = '#002B36'
+c.colors.tabs.selected.odd.bg = '#839496'
+c.colors.tabs.selected.odd.fg = '#002B36'
+c.colors.tooltip.bg = '#002B36'
+c.colors.tooltip.fg = '#93A1A1'
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.preferred_color_scheme = 'dark'
+c.content.autoplay = False
+c.content.javascript.enabled = False
+c.content.notifications.enabled = False
+c.downloads.position = 'bottom'
+c.fonts.default_family = 'Terminus'
+c.fonts.default_size = '14px'
+c.fonts.contextmenu = 'default_size default_family'
+c.fonts.prompts = 'default_size default_family'
+c.fonts.tooltip = 'default_size default_family'
+c.hints.border = '1px solid #2AA198'
+c.spellcheck.languages = ['en-US', 'el-GR']
+c.tabs.new_position.unrelated = 'next'
+c.tabs.select_on_remove = 'prev'
+c.window.hide_decoration = True
+c.url.searchengines = {
+    'DEFAULT': 'https://duckduckgo.com/?q={}',
+    'gh'     : 'https://github.com/search?q={}',
+    'g'      : 'https://google.com/search?hl=en&q={}',
+    'gt'     : 'https://translate.google.com/?hl=en&sl=auto&tl=en&text={}&op=translate',
+    'gr'     : 'https://translate.google.com/?hl=en&sl=auto&tl=el&text={}&op=translate',
+    'gm'     : 'https://google.com/maps?hl=en&q={}',
+    'om'     : 'https://openstreetmap.org/search?commit=Go&query={}',
+    'w'      : 'https://en.wikipedia.org/w/index.php?search={}',
+    'yt'     : 'https://youtube.com/results?search_query={}',
+    'iv'     : 'https://yewtu.be/search?q={}'
+}
+config.bind('(', 'set fonts.default_size "14px" ;; set zoom.default "100%"')
+config.bind(')', 'set fonts.default_size "18px" ;; set zoom.default "150%"')
+config.bind(',l', 'spawn sh -c "echo \'{url}\' >> /tmp/linklist"')
+config.bind(',m', 'spawn mpv --no-terminal --force-window=immediate --keep-open \'{url}\'')
+config.bind(',M', 'spawn mpv --ytdl-format="bestvideo+bestaudio/best" --no-terminal --force-window=immediate --keep-open \'{url}\'')
+config.bind(';i', 'hint images yank')
+config.bind(';I', 'hint images yank-primary')
+config.bind(';l', 'hint links spawn sh -c "echo {hint-url} >> /tmp/linklist"')
+config.bind(';m', 'hint links spawn mpv --no-terminal --force-window=immediate --keep-open \'{hint-url}\'')
+config.bind(';M', 'hint links spawn mpv --ytdl-format="bestvideo+bestaudio/best" --no-terminal --force-window=immediate --keep-open \'{hint-url}\'')
+config.bind('xt', 'open -t https://translate.google.com/translate?hl=en&sl=auto&tl=en&u={url}')
+config.bind('xT', 'open https://translate.google.com/translate?hl=en&sl=auto&tl=en&u={url}')
+config.bind('<Alt+h>', 'scroll-px -10 0')
+config.bind('<Alt+j>', 'scroll-px 0 10')
+config.bind('<Alt+k>', 'scroll-px 0 -10')
+config.bind('<Alt+l>', 'scroll-px 10 0')
+config.set('content.javascript.enabled', True, '*://*.codeberg.org/*')
+config.set('content.javascript.enabled', True, '*://*.gitea.com/*')
+config.set('content.javascript.enabled', True, '*://*.github.com/*')
+config.set('content.javascript.enabled', True, '*://*.gitlab.com/*')
+config.set('content.javascript.enabled', True, '*://*.inoreader.com/*')
+config.set('content.javascript.enabled', True, '*://*.openstreetmap.org/*')
+config.set('content.javascript.enabled', True, '*://*.proton.me/*')
+config.set('content.javascript.enabled', True, '*://translate.google.com/*')
+config.set('content.javascript.enabled', True, '*://www.google.com/maps/*')
