9fe2e23a3ea0e2cab8bcc07c28c5fab138f8504e
bitraid <bitraid@protonmail.ch>
2024-10-14 22:23:56 +0300

    qutebrowser: group items by adding empty lines

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,5 +1,7 @@
 config.load_autoconfig(False)
+
 c.backend = 'webengine'
+
 c.colors.completion.category.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #073642, stop:1 #002B36)'
 c.colors.completion.category.border.bottom = '#002B36'
 c.colors.completion.category.border.top = '#073642'
@@ -15,12 +17,14 @@ c.colors.completion.match.fg = '#2AA198'
 c.colors.completion.odd.bg = '#073642'
 c.colors.completion.scrollbar.bg = '#002B36'
 c.colors.completion.scrollbar.fg = '#657B83'
+
 c.colors.contextmenu.disabled.bg = '#002B36'
 c.colors.contextmenu.disabled.fg = '#586E75'
 c.colors.contextmenu.menu.bg = '#002B36'
 c.colors.contextmenu.menu.fg = '#93A1A1'
 c.colors.contextmenu.selected.bg = '#93A1A1'
 c.colors.contextmenu.selected.fg = '#002B36'
+
 c.colors.downloads.bar.bg = '#073642'
 c.colors.downloads.error.bg = '#002B36'
 c.colors.downloads.error.fg = '#D33682'
@@ -28,12 +32,15 @@ c.colors.downloads.start.bg = '#002B36'
 c.colors.downloads.start.fg = '#6C71C4'
 c.colors.downloads.stop.bg = '#002B36'
 c.colors.downloads.stop.fg = '#268BD2'
+
 c.colors.hints.bg = 'qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 rgba(7, 54, 66, 0.8), stop:1 rgba(0, 43, 54, 0.8))'
 c.colors.hints.fg = '#FDF6E3'
 c.colors.hints.match.fg = '#B58900'
+
 c.colors.keyhint.bg = 'rgba(0, 43, 53, 80%)'
 c.colors.keyhint.fg='#839496'
 c.colors.keyhint.suffix.fg = '#B58900'
+
 c.colors.messages.error.bg = '#073642'
 c.colors.messages.error.border = '#586E75'
 c.colors.messages.error.fg = '#DC322F'
@@ -43,10 +50,12 @@ c.colors.messages.info.fg = '#B58900'
 c.colors.messages.warning.bg = '#073642'
 c.colors.messages.warning.border = '#586E75'
 c.colors.messages.warning.fg = '#CB4B16'
+
 c.colors.prompts.bg = '#002B36'
 c.colors.prompts.border = '1px solid #586E75'
 c.colors.prompts.fg = '#93A1A1'
 c.colors.prompts.selected.bg = '#586E75'
+
 c.colors.statusbar.caret.bg = '#002b36'
 c.colors.statusbar.caret.fg = '#B58900'
 c.colors.statusbar.caret.selection.bg = '#002B36'
@@ -70,6 +79,7 @@ c.colors.statusbar.url.hover.fg = '#268BD2'
 c.colors.statusbar.url.success.http.fg = '#2AA198'
 c.colors.statusbar.url.success.https.fg = '#859900'
 c.colors.statusbar.url.warn.fg = '#D33682'
+
 c.colors.tabs.bar.bg = '#002B36'
 c.colors.tabs.even.bg = '#073642'
 c.colors.tabs.even.fg = '#839496'
@@ -91,24 +101,34 @@ c.colors.tabs.selected.even.bg = '#839496'
 c.colors.tabs.selected.even.fg = '#002B36'
 c.colors.tabs.selected.odd.bg = '#839496'
 c.colors.tabs.selected.odd.fg = '#002B36'
+
 c.colors.tooltip.bg = '#002B36'
 c.colors.tooltip.fg = '#93A1A1'
+
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.preferred_color_scheme = 'dark'
+
 c.content.autoplay = False
 c.content.javascript.enabled = False
 c.content.notifications.enabled = False
+
 c.downloads.position = 'bottom'
+
 c.fonts.default_family = 'Terminus'
 c.fonts.default_size = '14px'
 c.fonts.contextmenu = 'default_size default_family'
 c.fonts.prompts = 'default_size default_family'
 c.fonts.tooltip = 'default_size default_family'
+
 c.hints.border = '1px solid #2AA198'
+
 c.spellcheck.languages = ['en-US', 'el-GR']
+
 c.tabs.new_position.unrelated = 'next'
 c.tabs.select_on_remove = 'prev'
+
 c.window.hide_decoration = True
+
 c.url.searchengines = {
     'DEFAULT': 'https://duckduckgo.com/?q={}',
     'gh'     : 'https://github.com/search?q={}',
@@ -121,6 +141,7 @@ c.url.searchengines = {
     'yt'     : 'https://youtube.com/results?search_query={}',
     'iv'     : 'https://yewtu.be/search?q={}'
 }
+
 config.bind('(', 'set fonts.default_size "14px" ;; set zoom.default "100%"')
 config.bind(')', 'set fonts.default_size "18px" ;; set zoom.default "150%"')
 config.bind(',l', 'spawn sh -c "echo \'{url}\' >> /tmp/linklist"')
@@ -138,6 +159,7 @@ config.bind('<Alt+h>', 'scroll-px -10 0')
 config.bind('<Alt+j>', 'scroll-px 0 10')
 config.bind('<Alt+k>', 'scroll-px 0 -10')
 config.bind('<Alt+l>', 'scroll-px 10 0')
+
 config.set('content.javascript.enabled', True, '*://*.codeberg.org/*')
 config.set('content.javascript.enabled', True, '*://*.gitea.com/*')
 config.set('content.javascript.enabled', True, '*://*.github.com/*')
