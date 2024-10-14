7a5f5de5a4212749d21d2367aad674a220feca50
bitraid <bitraid@protonmail.ch>
2024-10-14 18:27:18 +0300

    sway/foot/tmux/fish: improve integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,17 @@
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block blink
+if test -t 0
+	set fish_cursor_default block
+	set fish_cursor_insert line
+	set fish_cursor_replace_one underscore
+	set fish_cursor_visual block blink
+	fish_hybrid_key_bindings
+end
 
-fish_hybrid_key_bindings
+if status is-login
+	set -gx EDITOR nvim
+	set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+	eval (dircolors -c "$HOME/.config/dircolors-solarized/dircolors.256dark")
+end
 
 if status is-interactive
 	set -x GPG_TTY (tty)
-	set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-	eval (dircolors -c "$HOME/.config/dircolors-solarized/dircolors.256dark")
 end
